193be67bbd4af41076a3fa76dec353824decd95f
June Epstein <epstein.june@gmail.com>
2017-09-06 10:31:52 -0400

    added basic React to the scripts file

---
+++ dev/scripts/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render(){
+		console.log('Engaged');
+		return (
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
